efa4eb5a48194ca73a8d103e45c508a0523ade73
Marcus-auz <oo7marcusaurelius@gmail.com>
2019-11-13 19:19:11 +0530

    final game logic added

--- js/Player.js
+++ js/Player.js
@@ -40,5 +40,10 @@ class Player {
      */
 	get activeToken() {
         return this.unusedTokens[0];
-	}
+    }
+    //if player has undropped toke left
+    
+    checkTokens(){
+        return this.unusedTokens.length==0?false:true;
+    }
 }
